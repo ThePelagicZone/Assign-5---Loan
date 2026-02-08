f34506b95403f8340e7517fefcece76c7a23c9a4
A Student <student@nowhere.edu>
2026-02-08 22:56:46 +0000

    Feb 8, 2026, 2:56 PM

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main() {
   cout << fixed << setprecision(2);
 
+  // Parameters
   double balance; 
   double monthlyPayment;
   double apr;
   int month = 0;
 
+  // Inputs
   cout << "Enter balance: ";
   cin >> balance;
 
@@ -18,5 +20,7 @@ int main() {
   cin >> monthlyPayment;
 
   cout << "Enter APR: ";
-  cin >> apr
+  cin >> apr;
+
+  // 
 }
